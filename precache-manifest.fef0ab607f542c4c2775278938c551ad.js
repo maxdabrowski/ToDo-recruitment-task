792233f86b9f054d920904a2dd95fa53be83472c
Max <maksymilian.dabrowski93@gmail.com>
2019-05-09 21:07:24 +0200

    Updates

---
+++ precache-manifest.fef0ab607f542c4c2775278938c551ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf4a4cb33a8bd94b13de",
+    "url": "/ToDo-recruitment-task/static/css/main.6d8759ff.chunk.css"
+  },
+  {
+    "revision": "cf4a4cb33a8bd94b13de",
+    "url": "/ToDo-recruitment-task/static/js/main.cf4a4cb3.chunk.js"
+  },
+  {
+    "revision": "77a458057ff8f8a54ef7",
+    "url": "/ToDo-recruitment-task/static/js/1.77a45805.chunk.js"
+  },
+  {
+    "revision": "253974ed94f239c8ea0e",
+    "url": "/ToDo-recruitment-task/static/js/runtime~main.253974ed.js"
+  },
+  {
+    "revision": "2e126b679be0f49fe3bcdc4019407b7e",
+    "url": "/ToDo-recruitment-task/index.html"
+  }
+];
